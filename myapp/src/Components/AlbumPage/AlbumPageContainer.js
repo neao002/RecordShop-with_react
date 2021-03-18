44f8b3a762f28d798f104c412e5fbb8d064a9977
muratcanoncu <murat.oncu1992@hotmail.com>
2021-03-18 11:56:57 +0100

    albumPageContainer useLocation

--- myapp/src/Components/AlbumPage/AlbumPageContainer.js
+++ myapp/src/Components/AlbumPage/AlbumPageContainer.js
@@ -1,33 +1,19 @@
-<<<<<<< HEAD
-import React from 'react'
-import Hero from './AlbumTemplateHero'
-import Carousel from './AlbumBottomSuggestions'
-import {useLocation} from 'react-router-dom'
-const AlbumPageContainer = () => {
-    let location = useLocation()
-    console.log(location.state)
-    const {artist,title} = location.state.album
-    return (
-        <div>
-            <p>album page container</p>
-
-            <p>{artist}</p>
-            <p>{title}</p>
-        </div>
-    )
-}
-=======
 import React from "react";
 import Hero from "./AlbumTemplateHero";
 import Carousel from "./AlbumBottomSuggestions";
-
+import { useLocation } from "react-router-dom";
 const AlbumPageContainer = () => {
+  let location = useLocation();
+  console.log(location.state);
+  const { artist, title } = location.state.album;
   return (
     <div>
       <p>album page container</p>
+
+      <p>{artist}</p>
+      <p>{title}</p>
     </div>
   );
 };
->>>>>>> 050fc06f1ea8277d0b225653f8ea9025b0524081
 
 export default AlbumPageContainer;
