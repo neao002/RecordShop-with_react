7ac2def12d7f242367d0cbf7bc1742891d1214fb
Your Name <you@example.com>
2021-03-22 14:17:11 +0100

    third commit Rodrigo

--- myapp/src/ContextFolder/AlbumsObject.js
+++ myapp/src/ContextFolder/AlbumsObject.js
@@ -137,6 +137,53 @@ export const Albums = [
     releaseYear: "1976",
     genre: "Rock",
     image: Eagles,
+    trackList: [
+      {
+        no: 1,
+        title: "Hotel California",
+        length: "6:30",
+      },
+      {
+        no: 2,
+        title: "New Kid in Town",
+        length: "5:04",
+      },
+      {
+        no: 3,
+        title: "Life in the Fast Lane",
+        length: "4:46",
+      },
+      {
+        no: 4,
+        title: "Wasted Time",
+        length: "4:55",
+      },
+      {
+        no: 5,
+        title: "Wasted Time (Reprise)",
+        length: "1:22",
+      },
+      {
+        no: 6,
+        title: "Victim of Love",
+        length: "4:11",
+      },
+      {
+        no: 7,
+        title: "Pretty Maids All in a Row",
+        length: "4:05",
+      },
+      {
+        no: 8,
+        title: "Try and Love Again",
+        length: "5:10",
+      },
+      {
+        no: 9,
+        title: "The Last Resort",
+        length: "7:25",
+      },
+    ],
   },
   {
     id: 14,
@@ -145,6 +192,53 @@ export const Albums = [
     releaseYear: "1969",
     genre: "Rock",
     image: Zeppelin,
+    trackList: [
+      {
+        no: 1,
+        title: "Good Times, Bad Times",
+        length: "2:47",
+      },
+      {
+        no: 2,
+        title: "Babe I'm Gonna Leave You",
+        length: "6:43",
+      },
+      {
+        no: 3,
+        title: "You Shook Me",
+        length: "6:30",
+      },
+      {
+        no: 4,
+        title: "Dazed and Confused",
+        length: "6:26",
+      },
+      {
+        no: 5,
+        title: "Your Time Is Gonna Come",
+        length: "4:35",
+      },
+      {
+        no: 6,
+        title: "Black Mountain Side",
+        length: "2:06",
+      },
+      {
+        no: 7,
+        title: "Communication Breakdown",
+        length: "2:30",
+      },
+      {
+        no: 8,
+        title: "I Can't Quit You Baby",
+        length: "4:43",
+      },
+      {
+        no: 9,
+        title: "How Many More Times",
+        length: "8:33",
+      },
+    ],
   },
   {
     id: 15,
@@ -153,6 +247,48 @@ export const Albums = [
     releaseYear: "1971",
     genre: "Rock",
     image: Zeppelins,
+    trackList: [
+      {
+        no: 1,
+        title: "Black Dog",
+        length: "4:54",
+      },
+      {
+        no: 2,
+        title: "Rock and Roll",
+        length: "3:40",
+      },
+      {
+        no: 3,
+        title: "The Battle of Evermore",
+        length: "5:51",
+      },
+      {
+        no: 4,
+        title: "Stairway to Heaven",
+        length: "8:02",
+      },
+      {
+        no: 5,
+        title: "Misty Mountain Hop",
+        length: "4:38",
+      },
+      {
+        no: 6,
+        title: "Four Sticks",
+        length: "4:44",
+      },
+      {
+        no: 7,
+        title: "Going to California",
+        length: "3:31",
+      },
+      {
+        no: 8,
+        title: "When the Levee Breaks",
+        length: "7:07",
+      },
+    ],
   },
   {
     id: 16,
@@ -161,6 +297,58 @@ export const Albums = [
     releaseYear: "1983",
     genre: "Rock",
     image: Synchronicity,
+    trackList: [
+      {
+        no: 1,
+        title: "Synchronicity I",
+        length: "3:20",
+      },
+      {
+        no: 2,
+        title: "Walking In Your Footsteps",
+        length: "3:34",
+      },
+      {
+        no: 3,
+        title: "O, My God",
+        length: "4:00",
+      },
+      {
+        no: 4,
+        title: "Mother",
+        length: "3:05",
+      },
+      {
+        no: 5,
+        title: "Miss Gradenko",
+        length: "1:58",
+      },
+      {
+        no: 6,
+        title: "Synchronicity II",
+        length: "4:59",
+      },
+      {
+        no: 7,
+        title: "Every Breath You Take",
+        length: "4:14",
+      },
+      {
+        no: 8,
+        title: "King of Pain",
+        length: "4:54",
+      },
+      {
+        no: 9,
+        title: "Wrapped Around Your Finger",
+        length: "4:54",
+      },
+      {
+        no: 10,
+        title: "Tea In The Sahara",
+        length: "4:06",
+      },
+    ],
   },
   {
     id: 17,
@@ -169,6 +357,68 @@ export const Albums = [
     releaseYear: "1991",
     genre: "Rock",
     image: Achtung,
+    trackList: [
+      {
+        no: 1,
+        title: "Interference",
+        length: "25:46",
+      },
+      {
+        no: 2,
+        title: "Even Better Than the Real Thing",
+        length: "3:41",
+      },
+      {
+        no: 3,
+        title: "Interference",
+        length: "25:46",
+      },
+      {
+        no: 4,
+        title: "Mysterious Ways",
+        length: "4:02",
+      },
+      {
+        no: 5,
+        title: "One(version 1)",
+        length: "4:34",
+      },
+      {
+        no: 6,
+        title: "The Fly",
+        length: "4:52",
+      },
+      {
+        no: 7,
+        title: "Interference",
+        length: "25:46",
+      },
+      {
+        no: 8,
+        title: "Even Better Than the Real Thing (dance mix)",
+        length: "4:35",
+      },
+      {
+        no: 9,
+        title: "One(version 2)",
+        length: "4:34",
+      },
+      {
+        no: 10,
+        title: "Even Better Than the Real Thing",
+        length: "3:45",
+      },
+      {
+        no: 11,
+        title: "One(version 3)",
+        length: "4:34",
+      },
+      {
+        no: 12,
+        title: "Until the End of the World",
+        length: "4:38",
+      },
+    ],
   },
   {
     id: 18,
@@ -177,6 +427,53 @@ export const Albums = [
     releaseYear: "1969",
     genre: "Rock",
     image: Stones,
+    trackList: [
+      {
+        no: 1,
+        title: "Gimme Shelter",
+        length: "4:31",
+      },
+      {
+        no: 2,
+        title: "Love in Vain",
+        length: "4:19",
+      },
+      {
+        no: 3,
+        title: "Country Honk",
+        length: "3:09",
+      },
+      {
+        no: 4,
+        title: "Live with Me",
+        length: "3:33",
+      },
+      {
+        no: 5,
+        title: "Let It Bleed",
+        length: "5:26",
+      },
+      {
+        no: 6,
+        title: "Midnight Rambler",
+        length: "6:52",
+      },
+      {
+        no: 7,
+        title: "You Got the Silver",
+        length: "2:51",
+      },
+      {
+        no: 8,
+        title: "Monkey Man",
+        length: "4:12",
+      },
+      {
+        no: 9,
+        title: "You Can't Always Get What You Want",
+        length: "7:28",
+      },
+    ],
   },
   {
     id: 19,
@@ -185,6 +482,78 @@ export const Albums = [
     releaseYear: "1965",
     genre: "Rock",
     image: Beatless,
+    trackList: [
+      {
+        no: 1,
+        title: "Drive My Car",
+        length: "2:25",
+      },
+      {
+        no: 2,
+        title: "Norwegian Wood This Bird Has Flown",
+        length: "2:04",
+      },
+      {
+        no: 3,
+        title: "You Won't See Me",
+        length: "3:26",
+      },
+      {
+        no: 4,
+        title: "Nowhere Man",
+        length: "2:44",
+      },
+      {
+        no: 5,
+        title: "Think for Yourself",
+        length: "2:19",
+      },
+      {
+        no: 6,
+        title: "The Word",
+        length: "2:44",
+      },
+      {
+        no: 7,
+        title: "Michelle",
+        length: "2:43",
+      },
+      {
+        no: 8,
+        title: "What Goes On",
+        length: "2:51",
+      },
+      {
+        no: 9,
+        title: "Girl",
+        length: "2:34",
+      },
+      {
+        no: 10,
+        title: "I'm Looking Through You",
+        length: "2:27",
+      },
+      {
+        no: 11,
+        title: "In My Life",
+        length: "2:30",
+      },
+      {
+        no: 12,
+        title: "Wait",
+        length: "2:20",
+      },
+      {
+        no: 13,
+        title: "If I Needed Someone",
+        length: "2:22",
+      },
+      {
+        no: 14,
+        title: "Run for Your Life",
+        length: "2:20",
+      },
+    ],
   },
   {
     id: 20,
@@ -193,6 +562,74 @@ export const Albums = [
     releaseYear: "1976",
     genre: "Rock",
     image: Ramones,
+    trackList: [
+      { no: 1, title: "Blitzkrieg Bop", length: "2:14" },
+      {
+        no: 2,
+        title: "Beat on the Brat",
+        length: "2:14",
+      },
+      {
+        no: 3,
+        title: "Judy is a Punk",
+        length: "1:32",
+      },
+      {
+        no: 4,
+        title: "I Wanna Be Your Boyfriend",
+        length: "2:24",
+      },
+      {
+        no: 5,
+        title: "Chain Saw",
+        length: "1:56",
+      },
+      {
+        no: 6,
+        title: "Now I Wanna Sniff Some Glue",
+        length: "1:35",
+      },
+      {
+        no: 7,
+        title: "I Don't Wanna Go Down to the Basement ",
+        length: "2:38",
+      },
+      {
+        no: 8,
+        title: "Loudmouth ",
+        length: "2:14",
+      },
+      {
+        no: 9,
+        title: "Havana Affair ",
+        length: "1:56",
+      },
+      {
+        no: 10,
+        title: "Listen to My Heart",
+        length: "1:58",
+      },
+      {
+        no: 11,
+        title: "53rd & 3rd",
+        length: "2:21",
+      },
+      {
+        no: 12,
+        title: "Let's Dance",
+        length: "1:51",
+      },
+      {
+        no: 13,
+        title: "I Don't Wanna Walk Around With You",
+        length: "1:42",
+      },
+      {
+        no: 14,
+        title: "Today Your Love, Tomorrow the World",
+        length: "2:12",
+      },
+    ],
   },
   {
     id: 21,
@@ -201,5 +638,32 @@ export const Albums = [
     releaseYear: "2017",
     genre: "Electro-Pop",
     image: Hideout,
+    trackList: [
+      {
+        no: 1,
+        title: "Myenemy",
+        length: "3:34",
+      },
+      {
+        no: 2,
+        title: "Gamesofluck",
+        length: "5:48",
+      },
+      {
+        no: 3,
+        title: Hideout,
+        length: "4:26",
+      },
+      {
+        no: 4,
+        title: "Older",
+        length: "3:27",
+      },
+      {
+        no: 5,
+        title: "Allaround",
+        length: "4:56",
+      },
+    ],
   },
 ];
